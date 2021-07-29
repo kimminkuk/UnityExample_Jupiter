6d1f0cadf5998e969aaa07718b0e35f0cdeb52a4
kimminkuk <rlaal567@gmail.com>
2021-07-30 00:44:53 +0900

    Modify Gladiator Stat

--- Object/Projectile.cs
+++ Object/Projectile.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Header("Movement Stuff")]
-    public float moveSpeed;
     public Vector2 directionToMove;
+    public FloatValue DamageProjectile;
+    public FloatValue ProjectileSpeed;
+    public float projectileSpeed_;
 
     [Header("Life Time")]
     public float lifetime;
@@ -20,18 +22,22 @@ public class Projectile : MonoBehaviour
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
     public float baseAttack;
-
+    private float moveSpeed = 4.999f;
     // Start is called before the first frame update
     void Start()
     {
+        projectileSpeed_ = ProjectileSpeed.RuntimeValue;
+        baseAttack = DamageProjectile.RuntimeValue;
         myRigidbody = GetComponent<Rigidbody2D>();
         lifetimeSeconds = lifetime;
+        Debug.Log("projectileSpeed_: " + projectileSpeed_);
     }
 
     // Update is called once per frame
     void Update()
     {
         lifetimeSeconds -= Time.deltaTime;
+
         if(lifetimeSeconds <= 0)
         {
             Destroy(this.gameObject);
@@ -40,7 +46,10 @@ public class Projectile : MonoBehaviour
 
     public void Launch(Vector2 initialVel)
     {
-        myRigidbody.velocity = initialVel * moveSpeed;
+        //myRigidbody.velocity = initialVel * moveSpeed;
+
+        projectileSpeed_ = ProjectileSpeed.RuntimeValue;
+        myRigidbody.velocity = initialVel * projectileSpeed_;
     }
 
     public virtual void OnTriggerEnter2D(Collider2D other)

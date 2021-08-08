4b3b26b18d892421c2e9778f12e698249b253dc9
kimminkuk <rlaal567@gmail.com>
2021-08-08 23:58:37 +0900

    Update BoolValue for OnOff Set

---
+++ ScriptableObject/BoolValue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+[System.Serializable]
+public class BoolValue : ScriptableObject
+{
+    public bool initialValue;
+    public bool RuntimeValue;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        RuntimeValue = initialValue;
+    }
+}
